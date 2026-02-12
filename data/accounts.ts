7f1cdfc42af433fdb58b630045182c1c65645867
hoaithanhsp <tolyhogroup@gmail.com>
2026-02-12 14:17:53 +0700

    Update accounts.ts

--- data/accounts.ts
+++ data/accounts.ts
@@ -1,5 +1,8 @@
 // File tài khoản đăng nhập - Thêm/xóa tài khoản tại đây
 export const accounts = [
     { username: "admin", password: "admin123", name: "Quản trị viên" },
+    { username: "chutrinhtayninh@gmail.com", password: "SKKN100", name: "Chu Lan Trinh" },
+    { username: "Xhuong91@gmail.com.", password: "SKKN100", name: "Nguyễn Thị Xuân Hương" },
     { username: "giaovien", password: "gv2025", name: "Giáo viên" },
 ];
+
