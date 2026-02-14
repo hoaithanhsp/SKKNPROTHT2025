dd4ee96d7e9b0a55f81253eac5e0b11c14befefb
hoaithanhsp <tolyhogroup@gmail.com>
2026-02-14 07:54:36 +0700

    Add new account for user 'lebaochau18042005@gmail.com'

--- data/accounts.ts
+++ data/accounts.ts
@@ -3,6 +3,7 @@ export const accounts = [
     { username: "admin", password: "admin123", name: "Quản trị viên" },
     { username: "chutrinhtayninh@gmail.com", password: "SKKN100", name: "Chu Lan Trinh" },
     { username: "duonghangdtntls@gmail.com", password: "SKKN100", name: "GV" },
+    { username: "lebaochau18042005@gmail.com", password: "SKKN100", name: "GV" },
     { username: "huonggiangpc89@gmail.com", password: "SKKN100", name: "GV" },
     { username: "dinhvidongthanh@gmail.com", password: "SKKN100", name: "GV" },
     { username: "Buihuusonkx@gmail.com", password: "SKKN100", name: "GV" },
@@ -16,6 +17,7 @@ export const accounts = [
     { username: "Xhuong91@gmail.com", password: "SKKN100", name: "Nguyễn Thị Xuân Hương" },
     { username: "giaovien", password: "gv2025", name: "Giáo viên" },
 ];
+
 
 
 
